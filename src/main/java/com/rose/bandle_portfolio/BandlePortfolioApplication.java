8770a3eaefb97e5cd6b39702bec29e7f45c5818b
rosieomusic <rosieomusic@gmail.com>
2026-01-15 11:39:35 -0500

    configured db connection

---
+++ src/main/java/com/rose/bandle_portfolio/BandlePortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.rose.bandle_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandlePortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandlePortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rose/bandle_portfolio/BandlePortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rose.bandle_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandlePortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
